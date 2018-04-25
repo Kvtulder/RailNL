a1b7dde14e8e450896040a34f1af53507c0c2940
tteulings <tijs.t@hotmail.com>
2018-04-25 11:08:05 +0200

    added run function, ability to set files and variables in main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os, sys
 directory = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(directory, "code"))
+sys.path.append(os.path.join(directory, "data"))
 sys.path.append(os.path.join(directory, "code", "objects"))
 sys.path.append(os.path.join(directory, "code", "algorithms"))
 
@@ -10,23 +11,33 @@ from alg_greedy import *
 from alg_random import *
 
 # prints tracks and total time
-def print_results(results):
+def print_results(algorithm, results):
     print("\n")
+    print(algorithm.__name__)
     for result in results[1]:
         print("{}".format(result))
     print("Score: :", results[0])
 
+def run(algorithm, stations, tracks):
+    num_of_lines = 7
+    max_duration = 120
+
+    results = algorithm(stations, tracks,num_of_lines, max_duration)
+
+    print_results(algorithm, results)
+
 # runs algorithms and logs results
 def main():
+    stations_file = "StationsHolland.csv"
+    tracks_file = "ConnectiesHolland.csv"
 
-    stations = load_stations()
-    tracks = load_tracks(stations)
+    stations = load_stations(stations_file)
+    tracks = load_tracks(tracks_file, stations)
 
-    greedy1_results = greedy1(stations, tracks, 22, 120)
-    print_results(greedy1_results)
+    run(greedy2, stations, tracks)
 
-    greedy2_results = greedy2(stations, tracks, 22, 120)
-    print_results(greedy2_results)
+    run(random2, stations, tracks)
+
 
 if __name__ == "__main__":
     main()
